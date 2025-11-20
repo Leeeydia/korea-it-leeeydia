598676eec3d5554cf3e5eb1bb8895539bd020ab8
Leeeydia <leeeydia@gmail.com>
2025-11-20 16:41:38 +0900

    11/20 소수판별기 문제 추가설명 필요

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,39 +1,42 @@
 package org.example;
 
-import java.awt.*;
-import java.util.Arrays;
-
 class Main {
     public static void main(String[] args) {
-        String msg = greet("Jin"); /// 문자열만 담을 수 있는 변수 msg에 greet 함수 실행 결과를 넣는다.
-        System.out.println(msg); /// Jin -> 인자
-// greet() -> return 값인 "Hello" + Jin + "!" 출력
 
-        Point point = createPoint(5, 10); /// Point 모양의 객체랑 연결 할 point 변수
-        System.out.println("Point create at (" + point.x + ", " + point.y + ")");
-// create() -> return 값 x,y 그대로 출력
-/// new Point 5,10
-        int[] evens = firstNEvenNumbers(5);
-        System.out.println("first 5 even numbers : " + Arrays.toString(evens));
-    }
+        boolean rs = Math.isEvenNumber(2);
+        System.out.println(rs);
+        // true
 
-    public static String greet(String name) {  /// 문자열 매개변수 1개 바든 greet 함수 선언
-        return "Hello, " + name + "!";
-    }
+        rs = Math.isEvenNumber(3);
+        System.out.println(rs);
+        // false
 
+        rs = Math.isEvenNumber(4);
+        System.out.println(rs);
+        // true
 
-    public static Point createPoint(int x, int y) {
-        return new Point(x, y);
-    }
-
-    public static int[] firstNEvenNumbers(int n) {
-        int[] evenNumbers = new int[n];
-
-        for (int i = 0, num = 2; i < n; i++, num += 2) {
-            evenNumbers[i] = num;
-        }
-        return evenNumbers;
+        rs = Math.isEvenNumber(5);
+        System.out.println(rs);
+        // false
++
     }
 }
 
-// 2의 배수만큼 증가                   vb
+class Math {
+    static boolean isEvenNumber(int num) {
+        if(num==1){
+            return false;
+
+        }
+        for (int i = 2; i <num; i++){
+            if(num% i ==0){
+
+            }
+        }
+        return true;
+    }
+}
+
+// 어떤 수가 소수인지 아닌지 판별하는 법
+// 1. 1은 소수가 아니다
+// 2.
