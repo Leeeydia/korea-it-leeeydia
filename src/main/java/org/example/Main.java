86f7b07fd95d1a6eba84aeb48bb2b036532b5c4e
Leeeydia <leeeydia@gmail.com>
2025-11-24 14:35:28 +0900

    11/24 상속을 통한 캐스팅 허용 문제풀이 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,12 +4,10 @@ package org.example;
 
 class Main {
     public static void main(String[] args) {
-        무기 a무기 = new 칼();
+        칼 a칼 = new 칼();
+        a칼 = new 활();
     }
 }
 
-class 무기 {
-
-}
-
-class 칼 extends 무기{};
+class 칼 { }
+class 활 extends 칼 { }
