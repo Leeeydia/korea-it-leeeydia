5b169b840e847472e57f70f8e7298fb3d262199e
Leeeydia <leeeydia@gmail.com>
2025-11-24 14:40:50 +0900

    11/24 무기 클래스로 만든 리모콘으로 칼 객체를 조종 문제풀이

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,18 @@
 package org.example;
 
-// 문제 : 아래가 실행되도록 해주세요.
-
 class Main {
     public static void main(String[] args) {
-        칼 a칼 = new 칼();
-        a칼 = new 활();
+        무기 a무기 = new 칼();
+        a무기.공격();
+        // 출력 : 칼로 공격합니다.
     }
 }
 
-class 칼 { }
-class 활 extends 칼 { }
+class 무기 {
+    void 공격(){}
+}
+class 칼 extends 무기{
+void 공격(){
+    System.out.println("칼로 공격합니다");
+}
+}
