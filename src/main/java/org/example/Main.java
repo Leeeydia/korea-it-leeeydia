c931f7e5c126c46895bf9bfa14bd0f7891deb4d8
Leeeydia <leeeydia@gmail.com>
2025-11-24 16:39:08 +0900

    11/24 서로 다른 3종 TV를 서로 다른 종류의 리모콘 3개로 컨트롤 문제풀이

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,57 +1,60 @@
 package org.example;
 
-// 문제 : 매개변수를 사용해서 전사가 매번 다르게 공격하도록 해주세요.
+// 문제 : 아래 코드가 실행되도록 해주세요.
 
 class Main {
     public static void main(String[] args) {
-        전사 a전사 = new 전사();
+        샤오미Tv a샤오미Tv = new 샤오미Tv();
+        삼성Tv a삼성Tv = new 삼성Tv();
+        LGTv aLGTv = new LGTv();
 
-        a전사.공격("브라이언", "칼");
-        // 브라이언이(가) 칼(으)로 공격합니다.
+        a샤오미Tv.켜기("샤오미Tv");
+        // 출력 => 샤오미Tv 켜집니다.
+        a샤오미Tv.끄기("샤오미Tv");
+        // 출력 => 샤오미Tv 꺼집니다.
+        a샤오미Tv.vr켜기("샤오미Tv");
+        // 출력 => 샤오미Tv vr켜기!
 
-        a전사.재공격();
-        // 브라이언이(가) 칼(으)로 공격합니다.
+        a삼성Tv.켜기("삼성Tv");
+        // 출력 => 삼성Tv 켜집니다.
+        a삼성Tv.끄기("삼성Tv");
+        // 출력 => 삼성Tv 꺼집니다.
+        a삼성Tv.ar켜기("삼성Tv");
+        // 출력 => 삼성Tv ar켜기!
 
-        a전사.공격("필립", "창");
-        // 필립이(가) 창(으)로 공격합니다.
-
-        a전사.공격("마크", "지팡이");
-        // 마크(가) 지팡이(으)로 공격합니다.
-
-        a전사.재공격();
-        // 마크(가) 지팡이(으)로 공격합니다.
-
-        a전사.재공격();
-        // 마크(가) 지팡이(으)로 공격합니다.
+        aLGTv.켜기("LGTv");
+        // 출력 => LGTv 켜집니다.
+        aLGTv.끄기("LGTv");
+        // 출력 => LGTv 꺼집니다.
+        aLGTv.게임모드전환("LGTv");
+        // 출력 => LGTv 게임모드전환!
     }
 }
 
-class 전사 {
-    // 데이터 오래 저장 지역변수보다는 전역변수 사용
-    // 전역변수 사용
-    String lastName;
-    String lastWeapon;
-
-    void 공격(String name, String weapon) {
-        System.out.println(name + "이(가)" + weapon + "(으)로 공격합니다.");
-        this.lastName = name;
-        this.lastWeapon = weapon;
+class TV {
+    void 켜기(String name) {
+        System.out.println(name+"켜집니다.");
     }
 
-    void 재공격() {
-        System.out.println(this.lastName + "이(가)" + this.lastWeapon + "(으)로 공격합니다.");
+    void 끄기(String name) {
     }
 }
 
 
-// 지역변수 전역변수
+class 샤오미Tv extends TV {
+    void vr켜기(String name) {
+        System.out.println(name + "꺼집니다.");
+    }
+}
 
+class 삼성Tv extends TV {
+    void ar켜기(String name) {
+        System.out.println(name + "ar켜기!");
+    }
+}
 
-
-
-
-
-
-// 이전의 공격 방식
-// 인스턴스 변수 복습
-// this 복습 28강 복습
+class LGTv extends TV {
+    void 게임모드전환(String name) {
+        System.out.println(name+"게임모드전환!");
+    }
+}
