598b91322600e66d0df27e8ca99fa3ae1accf557
Leeeydia <leeeydia@gmail.com>
2025-11-25 10:33:00 +0900

    11/25 38강 자동차 리모콘 문제풀이

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,30 @@
 package org.example;
 
-// 문제 : 정수 i가 가지고 있는 10을 double 형 변수에 넣고 해당 변수의 값을 다시 i에 넣는 코드를 작성해주세요.
+// 문제 : 자동차 리모콘이 페라리 객체를 가리키게 한 후 해당 리모콘이 가리키고 있는 객체를 다시 페라리 리모콘으로 가리키게(참조하게) 하는 코드를 작성해주세요.
 
 class Main {
     public static void main(String[] args) {
-        int i = 10;
-        double d =  i; // 여기선 자동형변환 허용
-        i =(int) d; // 여기선 자동형변환 불가능
+        자동차 a자동차 = new 자동차();
+        페라리 a페라리 = new 페라리();
+
+        a자동차.달리다();
+        a자동차.서다();
+        a페라리.뚜껑이_열리다();
+
     }
+}
+
+class 자동차 {
+    void 달리다() {
+    }
+
+    void 서다() {
+    }
+}
+
+class 페라리 extends 자동차 {
+    void 뚜껑이_열리다() {
+        System.out.println("페라리 뚜껑 열리다");
+    }
+
 }
