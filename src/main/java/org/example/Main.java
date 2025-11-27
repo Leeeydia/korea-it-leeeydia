a1620c750452ae1922050b6441b21e2298dec2ec
Leeeydia <leeeydia@gmail.com>
2025-11-27 15:32:10 +0900

    11/27 51강 인력관리소 문제풀이 (잠시 저장)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,7 @@ class Main {
     public static void main(String[] args) {
 
 
-        ArrayList<String>사람 = new ArrayList<>();
+        ArrayList<String> 사람 = new ArrayList<>();
         사람인력관리소 a사람인력관리소 = new 사람인력관리소();
 
         a사람인력관리소.add사람("홍길순", 33);
@@ -38,13 +38,21 @@ class Main {
 class 사람인력관리소 {
     int i;
 
+    // 여기 void 왜 써 ? static 왜 안써? -> new 실행하면 되기 때문에
     void add사람(String 이름, int 나이) {
-//        사람 a사람 = new 사람();
-//        a사람.번호 = this.i + 1;
-//        a사람.이름 = 이름;
-//        a사람.나이 = 나이;
+        //조립
+        사람 a사람 = new 사람();
+        a사람.번호 = this.i + 1;
+        a사람.이름 = 이름;
+        a사람.나이 = 나이;
 
-        System.out.println("나이가 " + 나이 + "살인 " + 3 + "번째 사람(" + 이름 + ")이 추가되었습니다.");
+        // 활용 (배열 사용 안 한 버전 추가 작성)
+
+
+        System.out.println("나이가 " + a사람.나이 + "살인 " + a사람.번호 + "번째 사람(" + a사람.이름 + ")이 추가되었습니다.");
+        a사람 = a사람;
+        this.i=a사람.번호;
+
     }
 
     사람 get사람(int 번호) {
@@ -54,9 +62,9 @@ class 사람인력관리소 {
 
 
 class 사람 {
-//int 번호;
-//int 나이;
-//String 이름;
+    int 번호;
+    int 나이;
+    String 이름;
 
     void 자기소개() {
 
