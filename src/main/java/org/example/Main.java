9f9b211c950aac0638116fde33ff67f9849d50fc
Leeeydia <leeeydia@gmail.com>
2025-11-27 12:05:24 +0900

    11/27 51강 인력관리소 문제풀이

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,43 +1,51 @@
+// 문제 : 아래가 실행되도록 해주세요.
+// 1. 배열 사용 X 버전
+// 2. 배열 사용 버전
+// 3. ArrayList 버전
+
 package org.example;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
+        사람인력관리소 a사람인력관리소 = new 사람인력관리소();
 
-        int[] arr = new int[10];
+        a사람인력관리소.add사람("홍길순", 33);
+        // 나이가 33살인 1번째 사람(홍길순)이 추가되었습니다.
+        a사람인력관리소.add사람("홍길동", 20);
+        // 나이가 20살인 2번째 사람(홍길동)이 추가되었습니다.
+        a사람인력관리소.add사람("임꺽정", 30);
+        // 나이가 30살인 3번째 사람(임꺽정)이 추가되었습니다.
 
-        Article[] articles = new Article[10];
+        사람 a사람1 = a사람인력관리소.get사람(1);
+        a사람1.자기소개();
+        // 저는 1번, 홍길순, 33살 입니다.
 
-        Article article1 = new Article();
-        articles[0] = article1;
+        사람 a사람2 = a사람인력관리소.get사람(2);
+        a사람2.자기소개();
+        // 저는 2번, 홍길동, 20살 입니다.
 
-        Article article2 = new Article();
-        articles[1] = article2;
-
-        System.out.println(article1.id);
-        System.out.println(article2.id);
-
-        System.out.println(articles[0].id);
-        System.out.println(articles[1].id);
+        사람 a사람3 = a사람인력관리소.get사람(3);
+        a사람3.자기소개();
+        // 저는 3번, 임꺽정, 30살 입니다.
     }
 }
 
-class Article {
-    static int lastId;
+class 사람인력관리소 {
+    int i;
 
-    int id;
-    String regDate;
-
-    static {
-        lastId = 0;
+    void add사람(String 이름, int 나이) {
+        this.i++;
+        System.out.println("나이가 " + 나이 + "살인 " + 3 + "번째 사람(" + 이름 + ")이 추가되었습니다.");
     }
 
-    Article() {
-        this(lastId+1,"2025-12-12 12:12:12"); // 다른 생성자 호출(실행), Constructor Call
-        lastId++;
+    사람 get사람(int 번호) {
+        return new 사람();
     }
+}
 
-    Article(int id, String regDate) {
-        this.id = id;
-        this.regDate = regDate;
+
+class 사람 {
+    void 자기소개() {
+
     }
 }
